fb589bfe61848cc18e947458303a7e471d4078a2
mukhammadnursulaiman <90449852+mukhammadnursulaiman@users.noreply.github.com>
2021-10-14 21:15:59 +0700

    Initial Commit

---
+++ Niaga_17.js
@@ -0,0 +1,10 @@
+var json_Niaga_17 = {
+"type": "FeatureCollection",
+"name": "Niaga_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": null, "REMARK": "Pusat Perdagangan Tradisional (Pasar Eceran/Grosir/Induk)" }, "geometry": { "type": "Point", "coordinates": [ 128.291447680000033, 2.050542840000048 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "REMARK": "Kantor Bank" }, "geometry": { "type": "Point", "coordinates": [ 128.292939202000071, 2.053076525000051 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "REMARK": "Pusat Perdagangan Tradisional (Pasar Eceran/Grosir/Induk)" }, "geometry": { "type": "Point", "coordinates": [ 128.560751165000056, 2.108721224000021 ] } }
+]
+}
